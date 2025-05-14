ac3e583e54e22aadf331b0b4f57029e3f67bdd80
Geornamezicek <jiri.vaclav.sticha@gmail.com>
2025-05-14 10:03:54 +0200

    inner styles aby se lukáš nepokakal

---
+++ QuizUser.js
@@ -0,0 +1,26 @@
+﻿function saveQuizToLocalStorage() {
+    const question = document.getElementById('question').value;
+    const selectedOption = document.querySelector('input[name="option"]:checked');
+
+    if (!question || !selectedOption) {
+        alert("Zadej otázku a vyber správnou odpověď.");
+        return;
+    }
+
+    const optionsText = {
+        A: document.getElementById('textA').value,
+        B: document.getElementById('textB').value,
+        C: document.getElementById('textC').value,
+        D: document.getElementById('textD').value
+    };
+
+    const quizItem = {
+        question: question,
+        options: optionsText,
+        correctAnswer: selectedOption.value
+    };
+
+    localStorage.setItem('quiz', JSON.stringify(quizItem));
+    alert("Uloženo!");
+}
+
