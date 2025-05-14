6376a646293ae42af39be38a2d7012b92875af94
Geornamezicek <jiri.vaclav.sticha@gmail.com>
2025-05-14 10:07:02 +0200

    inner js

--- QuizCollection.js
+++
@@ -1,5 +0,0 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    const quizData = JSON.parse(localStorage.getItem('quiz'));
-    console.log(quizData.question)
-    document.getElementById("item1").textContent = quizData.question;
-});
